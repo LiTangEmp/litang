c012653c743d9dfb2ac78785d1857b7fa4efa9ba
heyx <528239532@q.com>
2024-06-10 16:19:07 +0800

    MQ中service和impl分层

--- src/main/java/com/hyx/service/RabbitMQService.java
+++ src/main/java/com/hyx/service/RabbitMQService.java
@@ -13,22 +13,8 @@ import org.springframework.stereotype.Service;
  * 利用Spring框架的AmqpTemplate来简化消息发送的操作。
  */
 @Service
-public class RabbitMQService {
+public interface RabbitMQService {
 
-    private AmqpTemplate amqpTemplate;
-    private Jackson2JsonMessageConverter messageConverter;
-
-    @Autowired
-    public RabbitMQService(AmqpTemplate amqpTemplate , Jackson2JsonMessageConverter messageConverter) {
-        this.amqpTemplate = amqpTemplate;
-        this.messageConverter = messageConverter;
-    }
-
-
-    public void sendMessage(Message message) {
-        String jsonMessage = JSON.toJSONString(message);//使用FastjsonJSON.toJSONString()序列化消息
-        amqpTemplate.convertAndSend("messageQueue", jsonMessage);// 发送消息到名为“messageQueue”消息队列
-        System.out.println("发送消息: " + jsonMessage);
-    }
+void sendMessage(Message message);
 }
 
---
+++ src/main/java/com/hyx/service/impl/RabbitMQServiceImpl.java
@@ -0,0 +1,35 @@
+package com.hyx.service.impl;
+
+import com.alibaba.fastjson.JSON;
+import com.hyx.pojo.Message;
+import com.hyx.service.RabbitMQService;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * 用于发送消息到RabbitMQ。
+ * 利用Spring框架的AmqpTemplate来简化消息发送的操作。
+ */
+@Service
+public class RabbitMQServiceImpl implements RabbitMQService {
+
+    private AmqpTemplate amqpTemplate;
+    private Jackson2JsonMessageConverter messageConverter;
+
+    @Autowired
+    public void RabbitMQService(AmqpTemplate amqpTemplate , Jackson2JsonMessageConverter messageConverter) {
+        this.amqpTemplate = amqpTemplate;
+        this.messageConverter = messageConverter;
+    }
+
+
+    public void sendMessage(Message message) {
+        String jsonMessage = JSON.toJSONString(message);//使用FastjsonJSON.toJSONString()序列化消息
+        amqpTemplate.convertAndSend("messageQueue", jsonMessage);// 发送消息到名为“messageQueue”消息队列
+        System.out.println("发送消息: " + jsonMessage);
+    }
+}
+
+
